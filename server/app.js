3bee403024250f03899eb6ae6087895a82d8eada
ryans <ryansousis@gmail.com>
2024-07-31 05:14:52 -0400

    Some basic server loading

---
+++ server/app.js
@@ -0,0 +1,82 @@
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require("mongoose");
+const expressHandlebars = require('express-handlebars');
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
+const url = require('url');
+const redis = require('redis');
+const { hostname } = require('os');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const dbURL = 'mongodb+srv://ryansousis:056qshVZ8SMvvjpH@cluster0.l1cpjyk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+const mongooseOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+};
+
+mongoose.connect(dbURL, mongooseOptions, (err) => {
+    if(err){
+        console.log('Could not connect to database');
+        throw err;
+    }
+});
+
+let redisURL = {
+    hostname: 'redis-13881.c85.us-east-1-2.ec2.cloud.redislabs.com',
+    port: 13881,
+};
+
+let redisPass = 'mXIscZC3zuIQqpnGoPY2fLDdsIIivuhv';
+if(process.env.REDISCLOUD_URL){
+    redisURL = url.parse(process.env.REDISCLOUD_URL);
+    [, redisPass] = redisURL.auth.split(':');
+}
+const redisClient = redis.createClient({
+    host: redisURL.hostname,
+    port: redisURL.port,
+    password: redisPass,
+});
+
+const router = require('./router.js');
+
+const app = express();
+app.use('/assets', express.static(path.resolve(`${__dirname}/../../hosted`)));
+app.use(favicon(`${__dirname}/../../hosted/img/favicon.png`));
+app.disable('x-powered-by');
+app.use(compression());
+app.use(bodyParser.urlencoded({
+    extended:true,
+}));
+app.use(session({
+    key: 'sessionid',
+    store: new RedisStore({
+        client: redisClient
+    }),
+    secret: 'Something',
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        httpOnly:true,
+    },
+}));
+app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('views', `${__dirname}/../../views`);
+app.use(cookieParser());
+
+router(app);
+
+app.listen(port, (err) => {
+    if(err){
+        throw err;
+    }
+    console.log(`Listening on port ${port}`);
+});
